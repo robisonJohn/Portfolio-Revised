16cd4c22c210c697d8333f51ab4c7f4e47f1ae5b
johnrobison <robisonj8680@gmail.com>
2021-08-12 11:39:14 -0400

    final push before lunch

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-
+import Home from './screens/Home'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello there</h1>
+      <h1>hello there</h1>
     </div>
   );
 }
---
+++ client/src/screens/Home.jsx
@@ -0,0 +1,11 @@
+import { Layout } from '../components'
+
+const Home = () => {
+    return (
+        <Layout>
+            <h1>hello there</h1>
+        </Layout>
+    )
+}
+
+export default Home;
