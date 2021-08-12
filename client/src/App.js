de1afe08e1bb9ad8af4e7e079bfb1841db1e1f3c
johnrobison <robisonj8680@gmail.com>
2021-08-12 10:06:09 -0400

    added key dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import { getBooks } from './services/books' 
 
 function App() {
+
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const allBooks = await getBooks();
+      setBooks(allBooks);
+    };
+    fetchBooks()
+  }, []);
   return (
     <div className="App">
       <h1>Hello there</h1>
+      <div>
+        {books.map((book) => book.title)}
+      </div>
     </div>
   );
 }
