63d53ca05f45aad218a3a2ea7ae67d24d6a3b3fa
johnrobison <robisonj8680@gmail.com>
2021-08-12 10:54:46 -0400

    created layout

---
+++ client/src/components/Layout/Layout.jsx
@@ -0,0 +1,20 @@
+import './Layout.css';
+import Nav from "../Nav/Nav"
+import Footer from "../Footer/Footer"
+
+const Layout = (props) => {
+    return (
+        <div className="Layout">
+            <Nav />
+            <div className="layout-children">{props.children}</div>
+            <Footer />
+        </div>
+
+    )
+}
+
+export default Layout;
+
+
+
+
