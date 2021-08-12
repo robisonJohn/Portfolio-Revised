f52e472fd22904155fead956f002f2a3b8701a05
johnrobison <robisonj8680@gmail.com>
2021-08-12 10:32:30 -0400

    created Filter

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className="Footer">
+            <div id="copy-right">
+                Copyright © <span>John Robison</span> All Rights Reserved | {" "}
+                <a
+            href="https://github.com/robisonJohn"
+            target="_blank"
+            rel="noreferrer"
+                >
+                    See My Other Work
+                </a>{" "}
+            </div>
+            <div id="github-logo">
+            <a
+            href="https://github.com/robisonJohn/Portfolio-Revised"
+            target="_blank"
+            rel="noreferrer"
+            >
+                <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png"
+                width="30"
+                alt="github-logo"
+                />
+            </a>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,24 +1,8 @@
 import axios from 'axios';
 
-const getToken = () => {
-    return new Promise((resolve) => {
-        resolve(`Bearer ${localStorage.getItem('token') || null}`)
-    });
-};
-
 const api = axios.create({
     baseURL: 'http://localhost:3000/',
 });
 
-api.interceptors.request.request(
-    async function (config) {
-        config.headers['Authorization'] = await getToken();
-        return config;
-    },
-    function (error) {
-        console.log('Request error: ', error);
-        return Promise.reject(error)
-    }
-);
 
 export default api;
