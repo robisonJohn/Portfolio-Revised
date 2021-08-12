f5923c56f8e4ad0fbc3a611756adf8e70ecc392d
johnrobison <robisonj8680@gmail.com>
2021-08-12 12:47:55 -0400

    updated screens folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Home from './screens/Home'
+import Home from './screens/Home/Home'
 
 function App() {
 
   return (
     <div className="App">
       <h1>hello there</h1>
+      <h1>can I post two things?</h1>
     </div>
   );
 }
---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,9 @@
+import { Layout } from '../../components'
+
+const Home = () => {
+    return (
+        <h1>hello there</h1>
+    )
+}
+
+export default Home;
