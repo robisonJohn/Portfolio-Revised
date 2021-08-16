4985756f76893bfc9ae03d57ee6cc14f3ac5d9d4
johnrobison <robisonj8680@gmail.com>
2021-08-16 14:02:56 -0400

    final commits before deployment

--- client/src/screens/BookEdit/BookEdit.jsx
+++ client/src/screens/BookEdit/BookEdit.jsx
@@ -1,7 +1,17 @@
+import { useState, useEffect } from "react";
 
 const ItemEdit = (props) => {
     const [book, setBook] = useState({
-        
+        title: "",
+        description: "",
+        review: "",
+        img_url: "",
+        publication_date: "",
+        author: "",
+        category: "",
+        author_name: "",
+        category_name: "",
+    });
 
-    })
+    const [isUpdated, setUpdated] = useState(false);
 }
