1963853d1f287f58f55f251d55d93ebdfc15cee8
johnrobison <robisonj8680@gmail.com>
2021-08-16 14:23:07 -0400

    updated apiConfig file

--- client/src/screens/BookEdit/BookEdit.jsx
+++ client/src/screens/BookEdit/BookEdit.jsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
+import { getBook, updateBook } from "../../services/items";
+import { useParams, Redirect, Link } from "react-router-dom";
+import { Form, Image, Button, Container } from "react-bootstrap";
 
+/*
 const ItemEdit = (props) => {
     const [book, setBook] = useState({
         title: "",
@@ -14,4 +18,36 @@ const ItemEdit = (props) => {
     });
 
     const [isUpdated, setUpdated] = useState(false);
+    let { id } = useParams();
+
+    useEffect(() => {
+        const fetchBook = async () => {
+            const book = await getBook(id);
+            setBook(book);
+        };
+        fetchBook();
+    }, [id]);
+
+    const handleChange = (event) => {
+        const { title, value } = event.target;
+        setBook({
+            ...book,
+            [title]: value,
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const updated = await updateBook(id, book);
+        setUpdated(updated);
+    };
+
+    if (isUpdated) {
+        return <Redirect to={`/books/${id}`}/>
+    }
+
+    return ()
+
+
 }
+*/
--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://robison-portfolio-api.herokuapp.com/' : 'http://localhost:3000' 
+
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
-});
+    baseURL: baseUrl
+})
+
 
 
 export default api;
