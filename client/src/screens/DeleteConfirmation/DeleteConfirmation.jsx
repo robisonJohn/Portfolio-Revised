b678bb46e4d08f0804b7286558e43366222e7515
johnrobison <robisonj8680@gmail.com>
2021-08-17 12:12:42 -0400

    finalized styling for delete confirmation page

--- client/src/screens/DeleteConfirmation/DeleteConfirmation.jsx
+++ client/src/screens/DeleteConfirmation/DeleteConfirmation.jsx
@@ -4,18 +4,18 @@ import { Button, Container, Col, Row } from 'react-bootstrap';
 
 const DeleteConfirmation = () => {
     return (
-        <Container>
-            <h1>This book has been effectively deleted!</h1>
-            <h3>What would you like to do next?</h3>
+        <Container id="confirmation">
+            <h1 className="header">This book has been effectively deleted!</h1>
+            <h3 className="header">What would you like to do next?</h3>
             <Col>
-                <Row>
+                <Row className="row">
                     <Link to="/goodreads">
-                        <Button variant="info">Back to books</Button>
+                        <Button variant="info">Return to books</Button>
                     </Link>
                 </Row>
-                <Row>
+                <Row className="row">
                     <Link to="/">
-                        <Button variant="info">Back to home</Button>
+                        <Button variant="success">Return to home</Button>
                     </Link>
                 </Row>
             </Col>
